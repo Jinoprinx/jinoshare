7ff9e78a25d580b6f7d8c6b7d2a8abd0a7632572
jinoprinx <jino4rex@gmail.com>
2025-09-22 06:54:34 -0700

    2nd attempt Resolve vercel 'invalid type for function second argument in  posts/[id]/route.ts' error

--- packages/web/app/api/posts/[id]/route.ts
+++ packages/web/app/api/posts/[id]/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { get, remove, upsert } from "@/lib/db";
 import { ISharedPost } from "@jino/common";
 
-export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+export async function PUT(req: NextRequest, { params }: any) {
   const body = (await req.json()) as ISharedPost;
   const { id } = params;
   const existing = await get(id);
@@ -12,7 +12,7 @@ export async function PUT(req: NextRequest, { params }: { params: { id: string }
   return NextResponse.json({ post: updated });
 }
 
-export async function DELETE(_: NextRequest, { params }: { params: { id: string } }) {
+export async function DELETE(_: NextRequest, { params }: any) {
   const { id } = params;
   await remove(id);
   return NextResponse.json({ ok: true });
