81586ef6bb67e9df65d7f9d021ce3ffe9f600f13
jinoprinx <jino4rex@gmail.com>
2025-09-26 04:38:18 -0700

    Fixed Signup middleware error

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -28,6 +28,17 @@ async function main() {
 
   const app = express();
 
+  app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Origin', req.headers.origin);
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      res.header('Access-Control-Allow-Credentials', 'true');
+      return res.sendStatus(200);
+    }
+    next();
+  });
+
   app.use(cors({ 
     origin: [
       "https://jinoshare.vercel.app",
