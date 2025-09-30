4a6e66d00860ca4d6b98f1ae410f4ae6c85727d4
jinoprinx <jino4rex@gmail.com>
2025-09-30 08:28:26 -0700

    Redi scheduler backend 503 status fix

--- packages/api/src/scheduler/redis.ts
+++ packages/api/src/scheduler/redis.ts
@@ -3,5 +3,5 @@ import { config } from '../config';
 
 export const redisConnection = new IORedis(config.redisUrl as string, {
   maxRetriesPerRequest: null,
-  tls: {},
+  tls: { rejectUnauthorized: false },
 });
