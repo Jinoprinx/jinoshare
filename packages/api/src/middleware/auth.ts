f0f55ee036efb7e10a94b95ed127c3e72f2ae785
jinoprinx <jino4rex@gmail.com>
2025-10-07 03:38:37 -0700

    Fixed 'Unable to upload media' error

--- packages/api/src/middleware/auth.ts
+++ packages/api/src/middleware/auth.ts
@@ -34,6 +34,7 @@ export const protect = async (req: Request, res: Response, next: NextFunction) =
 };
 
 export const protectBearer = async (req: Request, res: Response, next: NextFunction) => {
+  console.log('protectBearer middleware called');
   const authHeader = req.headers.authorization;
 
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
